afbb210d6a5b7d141292162cb365e1bf11f50cff
Darius <darius.wohlan@t-online.de>
2020-10-28 18:12:34 +0100

    Added minimize

--- Soundboard/Form1.Designer.cs
+++ Soundboard/Form1.Designer.cs
@@ -160,10 +160,9 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Soundboard";
             this.Load += new System.EventHandler(this.Form1_Load);
